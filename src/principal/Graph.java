bde083742ba797062211cb1924c98e34426a8338
Oumar Marega <oumarmarega18@gmail.com>
2019-03-09 01:59:43 +0100

    Class abstrat java done

--- src/principal/Graph.java
+++ src/principal/Graph.java
@@ -1,4 +1,27 @@
 package principal;
 
+import java.util.Hashtable;
+import java.util.Vector;
+
 public abstract class Graph {
+    protected Numerotation numerotation;
+    public abstract Graph copyGraph();
+    public abstract boolean addVertex(Vertex v);
+    public abstract  boolean deleteVertex(Vertex v);
+    public abstract boolean addEgde(Vertex a, Vertex b);
+    public abstract boolean deleteEdge(Vertex a,Vertex b);
+    public abstract void readFromKeyBoard();
+    public abstract boolean readFromFile(String fileName);
+    public abstract void readFromGUI();
+    public abstract void displayOnConsole();
+    public abstract boolean writeToFile(String fileName);
+    public abstract void displayOnGUI();
+    public abstract Vector<Integer> getFS();
+    public abstract Vector<Hashtable<Vertex, Integer>> getAPS();
+    public abstract Vector<Vector<Edge>> getAdjMat();
+    public abstract Vector<Integer> getDDI();
+    public abstract boolean existEdge(Vertex a,Vertex b);
+    public abstract boolean existVertex(Vertex a,Vertex b);
+    public abstract double valueEdge(Vertex a, Vertex b);
+    public abstract double valueEdge(int a,int b);
 }
